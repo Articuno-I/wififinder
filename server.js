28144be8006963a676b517175b4a8895292ff497
Articuno-I <Articuno-I@users.noreply.github.com>
2016-11-07 22:44:13 +0000

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/edited_client.html');
+	res.sendFile(__dirname + '/client.html');
 });
 
 io.socket.on('connection').on('pm', function(data) {
+	//insert code here to escape html etc. Maybe put in bold, censor, etc.
 	io.sockets.sockets[data.to].emit('pm',{from: client.id, to: data.to, msg: data.msg});
 	io.socket.on('connection').emit('pm',{from: client.id, to: data.to, msg: data.msg});
 });
