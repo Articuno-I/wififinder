ecc68c96ecbb4787e74ed31ad3b917d156a337fc
Articuno-I <Articuno-I@users.noreply.github.com>
2016-11-09 23:46:30 +0000

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/client.html');
-});
+}); //may use something other than __dirname when it's eventually hosted, IDK how this'll work though
 
 app.get('/scripts.js', function(req,res) {
 	res.sendFile(__dirname + '/scripts.js');
@@ -14,12 +14,36 @@ app.get('/styles.css', function(req,res) {
 	res.sendFile(__dirname + '/styles.css');
 });
 
-io.socket.on('connection').on('pm', function(data) {
-	//insert code here to escape html etc. Maybe put in bold, censor, etc.
-	io.sockets.sockets[data.to].emit('pm',{from: client.id, to: data.to, msg: data.msg});
-	io.socket.on('connection').emit('pm',{from: client.id, to: data.to, msg: data.msg});
-	//need to code other connection-y things, I don't think this works so I'll rework it at some point
+o.on('connection', function(socket) {
+	socket.on('pm', function(data) {
+/*should do the following:
+1. search for socket in games
+2. get opponent's socket from here
+3. escape html, etc. Perhaps other formatting, censor swearing, etc.
+4. send message on to both sender and opponent
+*/
+	});
+	socket.on('request', function(data) {
+		//stuff for battle requests. Something along these lines:
+		var requesting = false;
+		for (var i = 0; i < battlerequests.length(); i++) {
+			if (battlerequests[i][requester] == socket) {
+				requesting = true;
+			}
+			if (!requesting) {
+				battlerequests.push({socket,data});
+//here need to add battle request to html, also need to find smogon username for this if not earlier, to make it human-readable among other things.
+			} else {
+//do stuff to stop them requesting multiple games at once
+			}
+		}
+	});
+	socket.on('challenge', function(data) {
+		//do stuff
+	});
+	//maybe add something to accept challenges?
 });
+
 
 http.listen(8000,function() {
 	console.log('listening on *:8000');
